7a90854e00dafb75724f08a0407575e7ec3771a5
Todd Kronenberg <toddkronenberg@gmail.com>
2015-07-21 11:53:52 -0700

    Added file path as title to page source code display

--- client/app/github/github.controller.js
+++ client/app/github/github.controller.js
@@ -10,9 +10,9 @@ angular.module('coderRead')
   $scope.github.branchname = 'master';
 
   $scope.getFile = function(index) {
-    var filepath = $scope.githubTree[index].path;
-    console.log('filepath', filepath);
-    Github.getFile($scope.github.username, $scope.github.reponame, filepath)
+    // $scope.filename = $scope.directoryStructure[index].match(/\w+[\W+\w+]*/)[0];
+    $scope.filepath = $scope.githubTree[index].path;
+    Github.getFile($scope.github.username, $scope.github.reponame, $scope.filepath)
     .then(function(data) {
       $scope.githubApi = data.data;
     });
